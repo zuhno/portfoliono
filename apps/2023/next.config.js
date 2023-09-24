cbe810aedfd246e8939d8085ac4f2a3443091396
cuttleman <silluat11@gmail.com>
2023-09-25 00:44:14 +0900

    test: env

--- apps/2023/next.config.js
+++ apps/2023/next.config.js
@@ -9,6 +9,11 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "src/common/styles")],
   },
+  env: {
+    ENV: process.env.ENV,
+    NODEMAILER_USER_EMAIL: process.env.NODEMAILER_USER_EMAIL,
+    NODEMAILER_USER_PASSWORD: process.env.NODEMAILER_USER_PASSWORD,
+  },
 };
 
 module.exports = nextConfig;
